979244fdb353daf76409b1e12a3d1df5777519de
Brandon Jones <bdjones2410@gmail.com>
2016-01-07 15:06:43 -0500

    cleaned up sass and organized it, secured javascript file

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-$('.upperbox').on('click',function(e){
-  e.preventDefault();
-  var sibs = $(this).parent().siblings().children(".upperbox");
-  var wideness= Math.round($(this).width()/$(this).parent().width()*100);
-  if(wideness > 60){
-      $(this).animate({width:"50%"},1000);
-  }else {
-    $(this).animate({width:"100%"},1000);
-  }
+(function() {
+  $(document).ready(function() {
+     page.init();
+ });
 
-  sibs.animate({width:"100%"},1000);
 
-  // ("shrunk");
-  // $(this).removeClass("twitch");
-  // sibs.removeClass("shrunk");
-  // sibs.addClass("twitch");
+  var page = {
 
-});
+    init:function(){
+      page.styling();
+      page.events();
+    },
+    styling:function(){
+
+    },
+    events:function(){
+      $('section').on('click','.upperbox',function(e){
+        e.preventDefault();
+        var sibs = $(this).parent().siblings().children(".upperbox");
+        var wideness= Math.round($(this).width()/$(this).parent().width()*100);
+        if(wideness > 60){
+            $(this).animate({width:"50%"},1000);
+        }else {
+          $(this).animate({width:"100%"},1000);
+        }
+        sibs.animate({width:"100%"},1000);
+      });
+    },
+  };
+
+}());
