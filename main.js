5b902273acf7f1160a9a4721049428e9c94ba5fa
Brandon Jones <bdjones2410@gmail.com>
2016-01-07 15:30:36 -0500

    made divs bounce once on mouse over

--- main.js
+++ main.js
@@ -14,6 +14,7 @@
 
     },
     events:function(){
+      //slider effect
       $('section').on('click','.upperbox',function(e){
         e.preventDefault();
         var sibs = $(this).parent().siblings().children(".upperbox");
@@ -25,7 +26,16 @@
         }
         sibs.animate({width:"100%"},1000);
       });
+        //bounce effect
+      $('section').on('mouseover','.upperbox',function(e){
+        e.preventDefault();
+        var wideness= Math.round($(this).width()/$(this).parent().width()*100);
+        if(wideness > 60){
+        $(this).animate({width:"99%"},200).animate({width:"100%"},200);
+      }
+      });
     },
   };
+
 
 }());
