00eb0cf3359041be0405fd749858bcf157762fa4
Brandon Jones <bdjones2410@gmail.com>
2016-01-11 14:29:38 -0500

    changed kninjanight pic, changed some slider properties, made them get bigger when selected, working on filling in phaser info

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
      page.init();
  });
 
-
+  var techhigh;
   var page = {
 
     init:function(){
@@ -11,32 +11,47 @@
       page.events();
     },
     styling:function(){
-      var scrnHeight = $(window).height();
-      $('section').css('height',scrnHeight+50);
-      $('.Projects').css('height',scrnHeight/4-25);
-
+      techhigh = $('.Projects').height();
+    //   var scrnHeight = $(window).height();
+    //   $('section').css('height',scrnHeight+50);
+    //   $('.Projects').css('height',scrnHeight/4-25);
+    //
     },
     events:function(){
       //slider effect
       $('section').on('click','.upperbox',function(e){
         e.preventDefault();
+        var that = $(this);
+        var biggertech = techhigh + 100;
         var sibs = $(this).parent().siblings().children(".upperbox");
         var wideness= Math.round($(this).width()/$(this).parent().width()*100);
         if(wideness > 60){
-            $(this).animate({width:"50%"},1000);
+
+            $(this).animate({width:"20%"},1000);
+            $(this).siblings('.lowerbox').animate({width:'80%'},1000);
+            $(this).parent('.Projects').animate({height:biggertech},1000);
+            $(this).children().toggleClass('hidden');
         }else {
           $(this).animate({width:"100%"},1000);
+          $(this).siblings('.lowerbox').animate({width:'40%'},1000);
+          $(this).parent('.Projects').animate({height:techhigh},1000);
+          $(this).children().toggleClass('hidden');
         }
         sibs.animate({width:"100%"},1000);
+        if($(this).parent().siblings().children('.upperbox').children('span').hasClass('hidden')){
+          $(this).parent().siblings().children('.upperbox').children().toggleClass('hidden');
+        }
+        $(this).parent().siblings().children('.lowerbox').animate({width:'40%'},1000);
+        $(this).parent('.Projects').siblings().animate({height:techhigh},1000);
       });
         //bounce effect
-      $('section').on('mouseover','.upperbox',function(e){
-        e.preventDefault();
-        var wideness= Math.round($(this).width()/$(this).parent().width()*100);
-        if(wideness > 60){
-        $(this).animate({width:"99%"},200).animate({width:"100%"},200);
-      }
-      });
+      // $('section').on('mouseover','.upperbox',function(e){
+      //   e.preventDefault();
+      //   var wideness= Math.round($(this).width()/$(this).parent().width()*100);
+      //   if(wideness > 60){
+      //   $(this).animate({width:"99%"},200).animate({width:"100%"},200);
+      // }
+      // });
     },
   };
 
