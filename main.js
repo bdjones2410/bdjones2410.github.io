d60d5c034d13abffdbab6292fec28bea2edce7bb
Brandon Jones <bdjones2410@gmail.com>
2016-01-11 14:38:07 -0500

    changed some styling removed left over divs

--- main.js
+++ main.js
@@ -12,17 +12,13 @@
     },
     styling:function(){
       techhigh = $('.Projects').height();
-    //   var scrnHeight = $(window).height();
-    //   $('section').css('height',scrnHeight+50);
-    //   $('.Projects').css('height',scrnHeight/4-25);
-    //
     },
     events:function(){
       //slider effect
       $('section').on('click','.upperbox',function(e){
         e.preventDefault();
         var that = $(this);
-        var biggertech = techhigh + 100;
+        var biggertech = techhigh + 200;
         var sibs = $(this).parent().siblings().children(".upperbox");
         var wideness= Math.round($(this).width()/$(this).parent().width()*100);
         if(wideness > 60){
@@ -44,14 +40,6 @@
         $(this).parent().siblings().children('.lowerbox').animate({width:'40%'},1000);
         $(this).parent('.Projects').siblings().animate({height:techhigh},1000);
       });
-        //bounce effect
-      // $('section').on('mouseover','.upperbox',function(e){
-      //   e.preventDefault();
-      //   var wideness= Math.round($(this).width()/$(this).parent().width()*100);
-      //   if(wideness > 60){
-      //   $(this).animate({width:"99%"},200).animate({width:"100%"},200);
-      // }
-      // });
     },
   };
 
