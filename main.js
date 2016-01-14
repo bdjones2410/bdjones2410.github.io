e54446f65c862652b7bca4a93f0c31d16934848d
Brandon Jones <bdjones2410@gmail.com>
2016-01-14 11:52:55 -0500

    fixed xindex click issue on projects

--- main.js
+++ main.js
@@ -15,6 +15,53 @@
         },
 
         events: function() {
+
+        $(document).on("scroll", onScroll);
+
+        //smoothscroll
+        $('a[href^="#"]').on('click', function (e) {
+            e.preventDefault();
+            $(document).off("scroll");
+
+            $('a').each(function () {
+                $(this).removeClass('active');
+            });
+            $(this).addClass('active');
+
+            var target = this.hash,
+                menu = target;
+            $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top+2
+            }, 500, 'swing', function () {
+                window.location.hash = target;
+                $(document).on("scroll", onScroll);
+            });
+        });
+
+
+          function onScroll(event){
+            var scrollPos = $(document).scrollTop();
+            $('nav a').each(function () {
+                var currLink = $(this);
+                var refElement = $(currLink.attr("href"));
+                if (refElement.position().top -200 <= scrollPos && refElement.position().top + refElement.height()-200 > scrollPos) {
+                    $('nav a .boxopt').removeClass("active");
+                    currLink.children('.boxopt').addClass("active");
+                }
+                else{
+                    currLink.removeClass("active");
+                }
+            });
+          }
+
+
+
+            //
+            // $('nav').on('click','.boxopt',function(e){
+            //   $(this).addClass('active');
+            //   $(this).parent().siblings().children('.boxopt').removeClass('active');
+            // });
             $('section').on('click', '.upperbox', function(e) {
                 e.preventDefault();
                 var biggertech = techhigh + 200;
